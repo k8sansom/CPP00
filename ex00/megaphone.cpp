ec475db7d7daae03ea351b59e760c373ad423b9c
Kate Sansom <katesansom@Kates-MacBook-Air.local>
2024-03-03 18:26:11 +0100

    ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -4,10 +4,7 @@
 int	main(int ac, char **av)
 {
 	if (ac < 2)
-	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-		return (1);
-	}
 	for (int i = 1; i < ac; i++)
 	{
 		for (int j = 0; av[i][j] != '\0'; j++)
