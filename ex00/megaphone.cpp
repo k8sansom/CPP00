57d366ad81893885fbc740b004d814cabfa9cc9e
Kate Sansom <katesansom@Kates-MacBook-Air.local>
2024-03-03 20:24:26 +0100

    updte

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -4,12 +4,12 @@
 int	main(int ac, char **av)
 {
 	if (ac < 2)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
-	for (int i = 1; i < ac; i++)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	for (int i = 1; av[i]; i++)
 	{
 		for (int j = 0; av[i][j] != '\0'; j++)
 			std::cout << (char)std::toupper(av[i][j]);
 	}
-	std::cout << "\n";
+	std::cout << std::endl;
 	return (0);
 }
