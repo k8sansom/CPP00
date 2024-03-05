fcd6c2924f7a2db7a6f0789a8b3fbe3411380c6c
Kathryn Sansom <ksansom@c4b12c4.42berlin.de>
2024-03-05 15:52:30 +0100

    updates

--- ex01/Contact.cpp
+++ ex01/Contact.cpp
@@ -1,7 +1,24 @@
 #include "Contact.hpp"
 
-int main()
+Contact::Contact{}
+
+Contact::~Contact{}
+
+std::string	Contact::_getInput(std::string str) const
 {
-	Contact instance;
-	return 0;
+	std::string	input = "";
+	bool		valid = false;
+	do
+	{
+		std::cout << str << std::flush;
+		std::getline(std::cin, input);
+		if (std::cin.good() && !input.empty())
+			valid = true;
+		else
+		{
+			std::cin.clear();
+			std::cout << "Invalid input, please try again." << std::endl;
+		}
+	} while (!valid);
+	return (input);
 }
--- ex01/Contact.hpp
+++ ex01/Contact.hpp
@@ -2,14 +2,26 @@
 # define CONTACT_HPP
 
 #include <iostream>
+#include <iomanip>
 
 class Contact
 {
 private:
-	/* data */
+	std::string	_firstName;
+	std::string	_lastName;
+	std::string	_nickname;
+	std::string	_phoneNumber;
+	std::string	_darkestSecret;
+	int			_index;
+	std::string	_printLen(std::string str) const;
+	std::string	_getInput(std::string str) const;
 public:
-	Contact(/* args */);
+	Contact();
 	~Contact();
+	void	init(void);
+	void	view(int index) const;
+	void	display(int index) const;
+	void	setIndex(int i);
 };
 
 Contact::Contact(void)
@@ -23,3 +35,4 @@ Contact::~Contact(void)
 	std::cout << "Destructor called" << std::endl;
 	return ;
 }
+#endif
