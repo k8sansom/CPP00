31a047265e4f9787a16f9c0cd903e6f44f194cec
Kate Sansom <katesansom@Kates-MacBook-Air.local>
2024-03-15 12:54:57 +0100

    adding ex02

---
+++ ex02/Account.cpp
@@ -0,0 +1,51 @@
+#include "Account.hpp"
+
+Account::Account( int initial_deposit ){
+	// std::cout << "Constructor called" << std::endl;
+	// return ;
+}
+
+Account::~Account( void ){
+	// std::cout << "Destructor called" << std::endl;
+	// return ;
+}
+
+static int	Account::getNbAccounts( void ) {
+
+}
+
+static int	Account::getTotalAmount( void ) {
+
+}
+
+static int	Account::getNbDeposits( void ) {
+
+}
+
+static int	Account::getNbWithdrawals( void ) {
+
+}
+
+static void	Account::displayAccountsInfos( void ) {
+    
+}
+
+void	Account::makeDeposit( int deposit ){
+
+}
+
+bool	Account::makeWithdrawal( int withdrawal ) {
+
+}
+
+int		Account::checkAmount( void ) const {
+
+}
+
+void	Account::displayStatus( void ) const {
+
+}
+
+void	Account::_displayTimestamp( void ) {
+
+}
